5ce51e27fd7f98177d7e1e37074bada21affd1f3
GeneAr1 <35385563+GeneAr1@users.noreply.github.com>
2018-01-29 10:49:52 -0500

    added exersice 2-10 to directory

---
+++ Out to Dinner 2-10.js
@@ -0,0 +1,16 @@
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+
+// your code goes here
+var bill;
+var total;
+var tip;
+
+bill = 10.25+3.99+7.15;
+
+tip = bill * .15;
+
+total = bill + tip;
+
+console.log("$" + total.toFixed(2));
