cbe2e7247821680861f3ece5eadbfb22df981f4e
GeneAr1 <35385563+GeneAr1@users.noreply.github.com>
2018-01-30 23:29:47 -0500

    Added Quiz 4-8 find my seat

---
+++ Find my seat quiz 4-8.js
@@ -0,0 +1,25 @@
+/*
+ * Programming Quiz: Find my Seat (4-8)
+ * 
+ * Write a nested for loop to print out all of the different seat combinations in the theater.
+ * The first row-seat combination should be 0-0 
+ * The last row-seat combination will be 25-99
+ * 
+ * Things to note: 
+ *  - the row and seat numbers start at 0, not 1
+ *  - the highest seat number is 99, not 100
+ */
+
+// Write your code here
+
+
+
+var row = 0;
+var seat = 0;
+for (var i = 0; i <= 25; i++) {
+    row = i;
+    for (var j = 0; j <= 99; j++) {
+        seat = j;
+        console.log(row + "-" + seat);
+    }
+}
